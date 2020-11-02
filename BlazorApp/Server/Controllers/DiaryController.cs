d21d4538b4da719c59c9a706bdbeea2c2fcb2fbb
Samir Ehsani <samirehsani19@gmail.com>
2020-11-02 22:54:17 +0100

    diary

--- BlazorApp/Server/Controllers/DiaryController.cs
+++ BlazorApp/Server/Controllers/DiaryController.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BlazorApp.Server.Models;
 using BlazorApp.Server.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BlazorApp.Server.Controllers
 {
-    [Route("api/v1.0/[Controller]")]
+    [Route("api/v1.0/[controller]")]
     [ApiController]
     public class DiaryController : Controller
     {
@@ -23,9 +25,54 @@ namespace BlazorApp.Server.Controllers
         [HttpGet("{id}")]
         public async Task<IActionResult>GetByID(int id)
         {
-            var diary=_diaryRepo.GetDiaryByID(id);
+            var diary=await _diaryRepo.GetDiaryByID(id);
+            if (diary==null)
+            {
+                return NotFound($"Diary with id: {id} could not be found");
+            }
             return Ok(diary);
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Post(Diary diary)
+        {
+            if (diary==null) { return StatusCode(StatusCodes.Status400BadRequest, "Model is empty"); }
+            
+            _diaryRepo.Add(diary);
+            if (await _diaryRepo.Save())
+            {
+                return Ok($"Diary with title {diary.Title} saved succecfully");
+            }
+            return StatusCode(StatusCodes.Status500InternalServerError, $"Database faulure!");
+        }
+
+        [HttpPut()]
+        public async Task<IActionResult> Put(Diary diary)
+        {
+            _diaryRepo.Update(diary); 
+            if(await _diaryRepo.Save())
+            {
+                return Ok($"Diary updated successfully");
+            }
+            return StatusCode(StatusCodes.Status500InternalServerError, $"Database failure");
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult>Delete(int id)
+        {
+            var diary = _diaryRepo.GetDiaryByID(id);
+            if (diary==null)
+            {
+                return NotFound();
+            }
+
+            _diaryRepo.Delete(diary);
+            if (await _diaryRepo.Save())
+            {
+                return Ok($"Diary with id: {id} deleted successfully");
+            }
+            return StatusCode(StatusCodes.Status500InternalServerError, $"Database failure");
+            
+        }
     }
 }
